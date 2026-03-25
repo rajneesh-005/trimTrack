bb42c28881a751491a48721ae205e200d8158c5a
Rajneesh Singh <rajneeshpersonal45@gmail.com>
2026-03-25 20:42:11 +0530

    improved home page design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
 
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <div>
-        <input type="url" id="org_url" className="bg-amber-50 border rounded-base block w-xl cursor-text px-2 py-2" 
+    <div className="flex items-center justify-center h-screen bg-black font-mono">
+      <div className="text-center">
+        <h1 className="font-extraboldld py-3 text-6xl text-amber-100">trimTrack</h1>
+        <h3 className="font-semibold text-amber-100 py-3 text-2xl">trim it, track it! </h3>
+        <input type="url" id="org_url" className="bg-amber-100 border rounded-lg block w-xl cursor-text px-2 py-2" 
           placeholder="paste url"
         ></input>  
       </div> 
