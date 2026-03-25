7cd64406110c6d734557bba81302491b4935ce9b
Rajneesh Singh <rajneeshpersonal45@gmail.com>
2026-03-25 19:28:36 +0530

    Started Developing Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div>
+        <input type="url" id="org_url" className="bg-amber-50 border rounded-base block w-xl cursor-text px-2 py-2" 
+          placeholder="paste url"
+        ></input>  
+      </div> 
+      <div>
+          <div></div>  
+          <button></button>  
+          <button></button>  
+      </div> 
+    </div>
+  )
+}
+
+export default App
