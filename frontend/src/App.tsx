0c8365c59639f4a040de31d3481ec6ab7a9182f2
Rajneesh Singh <rajneeshpersonal45@gmail.com>
2026-03-26 22:54:16 +0530

    improved ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 function App() {
 
   return (
-    <div className="flex items-center justify-center h-screen bg-black font-mono">
-      <div className="text-center">
-        <h1 className="font-extraboldld py-3 text-6xl text-amber-100">trimTrack</h1>
-        <h3 className="font-semibold text-amber-100 py-3 text-2xl">trim it, track it! </h3>
-        <input type="url" id="org_url" className="bg-amber-100 border rounded-lg block w-xl cursor-text px-2 py-2" 
-          placeholder="paste url"
-        ></input>  
-      </div> 
-      <div>
-          <div></div>  
-          <button></button>  
-          <button></button>  
+    <div className="flex flex-col items-center  h-screen bg-black font-mono ">
+        <div className="flex flex-col mt-5 ">
+          <h1 className="text-center font-extrabold py-3 text-6xl text-amber-100">trimTrack</h1>
+          <h3 className="text-center font-semibold text-amber-200 py-3 text-2xl">trim it, track it! </h3>
+          <input type="url" id="org_url" className="bg-amber-100 border rounded-lg block w-xl cursor-text px-2 py-2" 
+            placeholder="paste url"></input>  
+        </div> 
+
+
+      <div className="flex flex-row  gap-3 mt-4 w-xl ">
+          <div className="flex-1 text-black px-3 py-2 bg-amber-100 rounded-lg max-w-xl">
+            hello
+          </div>  
+          <div className="bg-amber-100 cursor-pointer p-2 rounded-lg font-semibold">save</div>  
+          <div className="bg-amber-100 cursor-pointer p-2 rounded-lg font-semibold">copy</div>  
       </div> 
     </div>
   )
