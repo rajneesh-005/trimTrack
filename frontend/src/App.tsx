d421c5df25d0c9b5f8cbb92cf187fd76e7d6ad89
Rajneesh Singh <rajneeshpersonal45@gmail.com>
2026-03-27 14:53:30 +0530

    added conditional rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,23 +18,30 @@ function App() {
     const data = await response.json();
     setShortUrl(data.data.short_url);
   }
+
+  const handleKeyDown = (event:React.KeyboardEvent<HTMLInputElement>) => {
+    if(event.key==='Enter'){
+      handleSave();
+    }
+  }
   return (
     <div className="flex flex-col items-center  h-screen bg-black font-mono ">
         <div className="flex flex-col mt-5 ">
           <h1 className="text-center font-extrabold py-3 text-6xl text-amber-100">trimTrack</h1>
           <h3 className="text-center font-semibold text-amber-200 py-3 text-2xl">trim it, track it! </h3>
           <input type="url" id="org_url" className="bg-amber-100 border rounded-lg block w-xl cursor-text px-2 py-2" 
-            placeholder="paste url" value={url} onChange={UrlSetter}></input>  
+            placeholder="paste url" value={url} onChange={UrlSetter} onKeyDown={handleKeyDown}></input>  
         </div> 
 
-
-      <div className="flex flex-row  gap-3 mt-4 w-xl ">
-          <div className="flex-1 text-black px-3 py-2 bg-amber-100 rounded-lg max-w-xl">
-            {shortUrl}
-          </div>  
-          <div className="bg-amber-100 cursor-pointer p-2 rounded-lg font-semibold" onClick={handleSave}>save</div>  
-          <div className="bg-amber-100 cursor-pointer p-2 rounded-lg font-semibold">copy</div>  
-      </div> 
+      {shortUrl &&
+        <div className="flex flex-row  gap-3 mt-4 w-xl ">
+            <div className="flex-1 text-black px-3 py-2 bg-amber-100 rounded-lg max-w-xl">
+              {shortUrl}
+            </div>  
+            <div className="bg-amber-100 cursor-pointer p-2 rounded-lg font-semibold" onClick={handleSave}>save</div>  
+            <div className="bg-amber-100 cursor-pointer p-2 rounded-lg font-semibold">copy</div>  
+        </div> 
+      }
     </div>
   )
 }
