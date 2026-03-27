aced1897d6be6db3535e2b20bc4ff54e8e010956
Rajneesh Singh <rajneeshpersonal45@gmail.com>
2026-03-27 23:13:58 +0530

    refactored clicks.ts

--- backend/src/db/queries/clicks.ts
+++ backend/src/db/queries/clicks.ts
@@ -2,8 +2,8 @@ import {pool} from '../index';
 import { ClickEvent } from '@shared/types/clicks';
 export async function insertClicks(data:ClickEvent){
     const insertedClick = await pool.query(
-        'INSERT INTO clicks(short_code,ip_address) VALUES ($1,$2) RETURNING *',
-        [data.short_code,data.ip_address]
+        'INSERT INTO clicks(short_code,ip_address,device,browser,os,referrer) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *',
+        [data.short_code,data.ip_address,data.device,data.browser,data.os,data.referrer]
     );
 
     return insertedClick.rows[0];
